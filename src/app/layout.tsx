c9f3d636d25960b23eee2e0366d0c0e78dba2ac8
robahtou <61357564+robahtou@users.noreply.github.com>
2025-11-10 16:44:09 -0600

    cleanup app.layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter }         from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css';
+
+
+const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' });
 
 export const metadata: Metadata = {
-  title: "Solace Candidate Assignment",
-  description: "Show us what you got",
+  title: 'Solace Candidate Assignment',
+  description: 'Show us what you got'
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${inter.variable} ${inter.className}`}>
+      <body>{children}</body>
     </html>
   );
 }
+
+
+export default RootLayout;
