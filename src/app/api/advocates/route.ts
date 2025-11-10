b8534e0b864d6568b9bb6be50e156835b57aa34b
robahtou <61357564+robahtou@users.noreply.github.com>
2025-11-10 17:29:55 -0600

    chore: react errors, typings, cleanup
    - react maps should have a 'key' prop
    - add typing to db schema and use in api routes and page component
    - fix html tag: 'th' cannot be a child of 'thead'. add 'tr'
    - add alias for utils and db schema

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,11 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import type { Advocate }  from '@DB/schema';
+
+import db                 from '@DB';
+import { advocates }      from '@DB/schema';
 
 export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
+  console.log(['GET /api/advocates']);
+  const data: Advocate[] = await db.select().from(advocates);
 
   return Response.json({ data });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,78 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
+import type { ChangeEvent }     from 'react';
+import type { Advocate }        from '@DB/schema';
 
-export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+import { useEffect, useState }  from 'react';
+import { filterAdvocates }      from '@Utils/filterAdvocates';
+
+function Home() {
+  const [advocates, setAdvocates]                 = useState<Advocate[]>([]);
+  const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
+  const [searchTerm, setSearchTerm]               = useState<string>('');
 
   useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
+    console.log('fetching advocates...');
+    fetch('/api/advocates').then((response) => {
+      response.json().then((jsonResponse: { data: Advocate[] }) => {
         setAdvocates(jsonResponse.data);
         setFilteredAdvocates(jsonResponse.data);
       });
     });
   }, []);
 
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const nextSearchTerm = e.target.value;
+    setSearchTerm(nextSearchTerm);
 
-    document.getElementById("search-term").innerHTML = searchTerm;
-
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
-      );
-    });
-
-    setFilteredAdvocates(filteredAdvocates);
+    const nextFiltered = filterAdvocates(advocates, nextSearchTerm);
+    setFilteredAdvocates(nextFiltered);
   };
 
-  const onClick = () => {
-    console.log(advocates);
+  const handleResetClick = () => {
+    console.log('resetting search...');
+    setSearchTerm('');
     setFilteredAdvocates(advocates);
   };
 
   return (
-    <main style={{ margin: "24px" }}>
+    <main style={{ margin: '24px' }}>
       <h1>Solace Advocates</h1>
       <br />
       <br />
       <div>
         <p>Search</p>
         <p>
-          Searching for: <span id="search-term"></span>
+          Searching for: <span>{searchTerm}</span>
         </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+        <input style={{ border: '1px solid black' }} value={searchTerm} onChange={handleSearchChange} />
+        <button onClick={handleResetClick}>Reset Search</button>
       </div>
       <br />
       <br />
       <table>
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>City</th>
+            <th>Degree</th>
+            <th>Specialties</th>
+            <th>Years of Experience</th>
+            <th>Phone Number</th>
+          </tr>
         </thead>
         <tbody>
           {filteredAdvocates.map((advocate) => {
             return (
-              <tr>
+              <tr key={advocate.id}>
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
                 <td>{advocate.city}</td>
                 <td>{advocate.degree}</td>
                 <td>
                   {advocate.specialties.map((s) => (
-                    <div>{s}</div>
+                    <div key={s}>{s}</div>
                   ))}
                 </td>
                 <td>{advocate.yearsOfExperience}</td>
@@ -89,3 +85,6 @@ export default function Home() {
     </main>
   );
 }
+
+
+export default Home;
