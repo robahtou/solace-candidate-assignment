3ad88d6e2ea2704f4dbcf3d270c37238c68699fc
robahtou <61357564+robahtou@users.noreply.github.com>
2025-11-10 18:57:14 -0600

    put the favicon in the public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,16 @@ export const metadata: Metadata = {
 function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en" className={`${inter.variable} ${inter.className}`}>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Solace" />
+
+        <link rel="manifest"          href="/favicon/manifest.json" />
+        <link rel="icon"              href="/favicon/favicon.ico"    type="image/x-icon"   sizes="48x48"   />
+        <link rel="icon"              href="/favicon/icon.svg"       type="image/svg+xml"  sizes="any"     />
+        <link rel="icon"              href="/favicon/icon.png"       type="image/png"      sizes="96x96"   />
+        <link rel="apple-touch-icon"  href="/favicon/apple-icon.png" type="image/png"      sizes="180x180" />
+      </head>
+
       <body>{children}</body>
     </html>
   );
